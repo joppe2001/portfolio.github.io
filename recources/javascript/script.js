5ba65c1187385335eaf9b1b3a1b39eda1a7165e1
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2022-05-27 17:11:53 +0200

    planets done

--- recources/javascript/script.js
+++ recources/javascript/script.js
@@ -1,11 +1,49 @@
-var sidePlanetContainer = document.getElementById("test")
-var sidePlanet = document.getElementById("planet-side")
+var sidePlanetContainer1 = document.getElementById("planet1")
+var sidePlanetContainer2 = document.getElementById("planet2")
+var sidePlanetContainer3 = document.getElementById("planet3")
+var sidePlanetContainer4 = document.getElementById("planet4")
+var sidePlanetContainer5 = document.getElementById("planet5")
+var sidePlanet1 = document.getElementById("planet-side1")
+var sidePlanet2 = document.getElementById("planet-side2")
+var sidePlanet3 = document.getElementById("planet-side3")
+var sidePlanet4 = document.getElementById("planet-side4")
+var sidePlanet5 = document.getElementById("planet-side5")
 
-sidePlanetContainer.addEventListener('mousedown', function() {
+
+
+sidePlanetContainer1.addEventListener('mousedown', function() {
     this.style.top = '75%'
     this.style.opacity = "1"
 });
+sidePlanetContainer2.addEventListener('mousedown', function() {
+    this.style.top = '65%'
+    this.style.opacity = "1"
+});
+sidePlanetContainer3.addEventListener('mousedown', function() {
+    this.style.top = '15%'
+    this.style.opacity = "1"
+});
+sidePlanetContainer4.addEventListener('mousedown', function() {
+    this.style.top = '80%'
+    this.style.opacity = "1"
+});
+sidePlanetContainer5.addEventListener('mousedown', function() {
+    this.style.top = '15%'
+    this.style.opacity = "1"
+});
 
-sidePlanet.addEventListener('mousedown', function() {
+sidePlanet1.addEventListener('mousedown', function() {
+    this.style.width = '15vh'
+})
+sidePlanet2.addEventListener('mousedown', function() {
+    this.style.width = '15vh'
+})
+sidePlanet3.addEventListener('mousedown', function() {
+    this.style.width = '15vh'
+})
+sidePlanet4.addEventListener('mousedown', function() {
+    this.style.width = '15vh'
+})
+sidePlanet5.addEventListener('mousedown', function() {
     this.style.width = '15vh'
 })
